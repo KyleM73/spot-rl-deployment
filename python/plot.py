693d334ec58f3ca4f7f917701f9eebbf678e9453
KyleM73 <kyle.morgenstein@gmail.com>
2024-11-14 12:47:42 -0600

    cli for start and stop idx for plotting

--- python/plot.py
+++ python/plot.py
@@ -13,16 +13,18 @@ def main() -> None:
     parser.add_argument("pickle_prefix", type=str)
     parser.add_argument("--history", type=int, default=1)
     parser.add_argument("--estimate", action="store_true", default=False)
+    parser.add_argument("--start_idx", type=int, default=0)
+    parser.add_argument("--stop_idx", type=int, default=-1)
     options = parser.parse_args()
 
     conf_file = isaaclab.isaaclab_configuration.detect_config_file(options.policy_file_path)
     config = isaaclab.isaaclab_configuration.load_configuration(conf_file)
 
     policy_abs_path = os.path.abspath(options.policy_file_path)
-    re = f"{policy_abs_path}/logs/{options.pickle_prefix}*.pkl"
-    plotter = Plotter(config, re, options.history, options.estimate)
+    regex = f"{policy_abs_path}/logs/{options.pickle_prefix}*.pkl"
+    plotter = Plotter(config, regex, options.history, options.estimate)
     plotter.merge_data()
-    plotter.plot(None)
+    plotter.plot([options.start_idx, options.stop_idx])
 
 
 if __name__ == "__main__":
